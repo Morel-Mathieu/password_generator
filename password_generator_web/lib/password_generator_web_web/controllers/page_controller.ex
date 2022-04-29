b7cbeadfa6e8fb76e44a061d82e3178277920b8c
Mathieu Morel <mathieu.morel@etu.uca.fr>
2022-04-29 14:00:45 +0200

    Layout and information from Controller

--- password_generator_web/lib/password_generator_web_web/controllers/page_controller.ex
+++ password_generator_web/lib/password_generator_web_web/controllers/page_controller.ex
@@ -2,6 +2,19 @@ defmodule PasswordGeneratorWebWeb.PageController do
   use PasswordGeneratorWebWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    password_lengths = [
+      Faible: Enum.map(6..15, & &1),
+      Fiable: Enum.map(16..88, & &1),
+      Impossible: [100, 150]
+    ]
+
+    password = ""
+
+    render(
+      conn,
+      "index.html",
+      password_length: password_lengths,
+      password: password
+    )
   end
 end
